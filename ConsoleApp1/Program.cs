2267b0d8c8a979cc39ade1b18647bde80540ddf6
Markus Bux <m.bux@outlook.de>
2020-09-10 15:12:33 +0200

    Add Some other parts

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Validation;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var validator = new NotDefaultAttribute();
+
+            var isValid1 = validator.IsValid(Guid.Empty);
+            var isValid2 = validator.IsValid(DateTime.MinValue);
+            var isValid3 = validator.IsValid(new int());
+            var isValid4 = validator.IsValid(new bool());
+            isValid1 = validator.IsValid(Guid.NewGuid());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WebApplication1/Validation/NotDefaultAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Validation
+{
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class NotDefaultAttribute : ValidationAttribute
+    {
+        public const string DefaultErrorMessage = "The {0} field must not have the default value";
+        public NotDefaultAttribute() : base(DefaultErrorMessage) { }
+
+        public override bool IsValid(object value)
+        {
+            // NotDefault doesn't necessarily mean required
+            // Therefire to check if it is not null
+            // use the [Required] attribute
+            if (value is null)
+            {
+                return true;
+            }
+
+            var type = value.GetType();
+            if (type.IsValueType)
+            {
+                var defaultValue = Activator.CreateInstance(type);
+                return !value.Equals(defaultValue);
+            }
+
+            // non-null ref type and therefore not defaut.
+            return true;
+        }
+    }
+}
